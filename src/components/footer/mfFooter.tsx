00d6c6650203d28f9734c89e64db07ef1c32bfe9
Iréne Forssell Nilsson <toplizard@gmail.com>
2026-01-25 07:47:05 +0100

    Refactor styles and improve component structure across multiple files

--- src/components/footer/mfFooter.tsx
+++ src/components/footer/mfFooter.tsx
@@ -6,6 +6,8 @@ import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import styles from './mfFooter.module.scss';
 import ThemeToggle from '@/components/theme-toggle/theme-toggle';
+import H from '@/components/hTag/mfHtag';
+import { HeadingLevels, HSizes } from '@/types/componentTypes';
 
 const storyblokApi = getStoryblokApi();
 
@@ -23,45 +25,65 @@ const MfFooter = async () => {
 		});
 
 	return (
-		<footer className={styles.mfFooter}>
-			<RichText
-				className={styles['mfFooter__content']}
-				content={(global?.story?.content as any)?.content?.content || []}
-			/>
+		<>
+			<footer className={styles.mfFooter}>
+				<div className={styles.mfFooter__container}>
+					<div className={styles['mfFooter__info']}>
+						<H
+							tag={global?.story?.content.headingLevel as HeadingLevels}
+							size={global?.story?.content.headingSize as HSizes}
+							suffix={global?.story?.content.titleSuffix as string}
+						>
+							{global?.story?.content.title || 'Footer'}
+						</H>
+						<RichText
+							content={(global?.story?.content as any)?.content?.content || []}
+						/>
+					</div>
 
-			<div className={styles['mfFooter__links']}>
-				<span>
-					<MfLink
-						href={global?.story?.content.email?.url || '/'}
-						icon={faEnvelope}
-					>
-						{global?.story?.content.email?.title || 'Email'}
-					</MfLink>
-				</span>
-				<span>
-					<MfLink
-						href={global?.story?.content.phone?.url || '/'}
-						icon={faPhone}
-					>
-						{global?.story?.content.phone?.title || 'Phone'}
-					</MfLink>
-				</span>
-				<span>
-					<MfLink
-						href={global?.story?.content.social?.url || '/'}
-						target={global?.story?.content.social?.target || '_blank'}
-						rel="noopener noreferrer"
-						icon={faLinkedin}
-					>
-						{global?.story?.content.social?.title || 'Social'}
-					</MfLink>
-				</span>
-			</div>
+					<div className={styles['mfFooter__links']}>
+						<span>
+							<MfLink
+								href={global?.story?.content.email?.url || '/'}
+								icon={faEnvelope}
+								variant="contact"
+							>
+								{global?.story?.content.email?.title || 'Email'}
+							</MfLink>
+						</span>
+						<span>
+							<MfLink
+								href={global?.story?.content.phone?.url || '/'}
+								icon={faPhone}
+								variant="contact"
+							>
+								{global?.story?.content.phone?.title || 'Phone'}
+							</MfLink>
+						</span>
+						<span className={styles['mfFooter__social-link']}>
+							<MfLink
+								href={global?.story?.content.social?.url || '/'}
+								target={global?.story?.content.social?.target || '_blank'}
+								rel="noopener noreferrer"
+								icon={faLinkedin}
+								variant="contact"
+								showValue={false}
+							>
+								{global?.story?.content.social?.title || 'Social'}
+							</MfLink>
+						</span>
+					</div>
+				</div>
+			</footer>
 			{
 				//only show theme toggle if not in production
-				process.env.NODE_ENV !== 'production' && <ThemeToggle />
+				process.env.NODE_ENV !== 'production' && (
+					<div className={styles['mfFooter__theme-toggle']}>
+						<ThemeToggle />
+					</div>
+				)
 			}
-		</footer>
+		</>
 	);
 };
 
